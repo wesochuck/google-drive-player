adf8a3e61c214a25044374e46380de0a1d068c1f
Wes Osborn <wosborn@clcohio.org>
2026-05-10 22:01:15 -0400

    chore: update vite base path to root directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/google-drive-player/',
+  base: '/',
   test: {
     globals: true,
     environment: 'jsdom',
